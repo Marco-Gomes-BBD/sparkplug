647aad063bd089fdaed05d702b254146a3e11f62
Marco Gomes <marcog@bbd.co.za>
2023-07-18 11:20:25 +0200

    Initial gui login

--- ritual_azure.py
+++ ritual_azure.py
@@ -1,16 +1,30 @@
 import subprocess
 import pyperclip
 
-command = ["aws-azure-login.cmd"]
-process = subprocess.Popen(
-    command,
-    stdin=subprocess.PIPE,
-    text=True,
-)
 
-# Write input to the process
+def console_login(password):
+    command = ["aws-azure-login.cmd"]
+    process = subprocess.Popen(
+        command,
+        stdin=subprocess.PIPE,
+        text=True,
+    )
+
+    # Write input to the process
+    process.stdin.write(f"${password}\n")
+    process.stdin.flush()
+    process.wait()
+
+
+def gui_login(password):
+    cmd = ["aws-azure-login.cmd", "--mode=gui"]
+    process = subprocess.Popen(cmd)
+
+    process.wait()
+    print(len(password) * "*")
+
+
 password = pyperclip.paste()
 password.strip("\n\t ")
-process.stdin.write(f"${password}\n")
-process.stdin.flush()
-process.wait()
+
+console_login(password)
