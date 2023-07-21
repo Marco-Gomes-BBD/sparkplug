ab37d44bab5d6ea43044c6750391a319501c7699
Marco Gomes <marcog@bbd.co.za>
2023-07-21 12:24:49 +0200

    Implement AWS gui login

--- ritual_azure.py
+++ ritual_azure.py
@@ -1,5 +1,15 @@
-import subprocess
+import os
+import time
+
 import pyperclip
+
+import pyautogui as ag
+import pygetwindow as gw
+import subprocess
+
+next_image = os.path.join("res", "next2.png")
+
+login_window_name = "Sign in to your account"
 
 
 def console_login(password):
@@ -11,20 +21,69 @@ def console_login(password):
     )
 
     # Write input to the process
-    process.stdin.write(f"${password}\n")
+    process.stdin.write(password)
+    process.stdin.write("\n")
     process.stdin.flush()
     process.wait()
+
+
+def getElementBounds(box):
+    bounds = None
+    if box is not None:
+        left, top, width, height = box.left, box.top, box.width, box.height
+        bounds = (left, top, width, height)
+    return bounds
+
+
+def getCenter(bounds):
+    left, top, width, height = bounds
+    x, y = left + width // 2, top + height // 2
+    return x, y
+
+
+def getElementCenter(element):
+    bounds = getElementBounds(element)
+    x, y = getCenter(bounds)
+    return x, y
+
+
+def typeWaitElement(image, text, bounds, searchTime: int = 60):
+    element = ag.locateOnScreen(image, region=bounds, minSearchTime=searchTime)
+    ag.typewrite(text)
+    if element is not None:
+        x, y = getElementCenter(element)
+        ag.click(x, y)
+
+
+def wait_login_window(name: str, timeout: float):
+    login_window = None
+    start_time = time.time()
+    while login_window is None:
+        windows = gw.getWindowsWithTitle(name)
+
+        if len(windows) == 1:
+            login_window = windows[0]
+
+        current_time = time.time() - start_time
+        if current_time >= timeout:
+            break
+        time.sleep(1 / 50)
+    return login_window
 
 
 def gui_login(password):
     cmd = ["aws-azure-login.cmd", "--mode=gui"]
     process = subprocess.Popen(cmd)
 
+    window = wait_login_window(login_window_name, 30)
+
+    window = gw.getActiveWindow()
+    if window.title == login_window_name:
+        bounds = getElementBounds(window)
+        typeWaitElement(next_image, password, bounds)
+
     process.wait()
-    print(len(password) * "*")
 
 
 password = pyperclip.paste()
-password.strip("\n\t ")
-
-console_login(password)
+gui_login(password)
